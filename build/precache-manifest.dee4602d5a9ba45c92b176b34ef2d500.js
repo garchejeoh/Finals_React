5fe181143c280642e9d5382182e85556da7afbca
Jeoh Garche <jeohgarche@gmail.com>
2022-05-30 15:39:26 +0800

    Update

---
+++ build/precache-manifest.dee4602d5a9ba45c92b176b34ef2d500.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dca7421f78e62b6545af",
+    "url": "/Garche_ReactActivity/static/css/main.95c03b37.chunk.css"
+  },
+  {
+    "revision": "dca7421f78e62b6545af",
+    "url": "/Garche_ReactActivity/static/js/main.dca7421f.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/Garche_ReactActivity/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "991833622dfb98c5b2a5",
+    "url": "/Garche_ReactActivity/static/css/2.ae513e08.chunk.css"
+  },
+  {
+    "revision": "991833622dfb98c5b2a5",
+    "url": "/Garche_ReactActivity/static/js/2.99183362.chunk.js"
+  },
+  {
+    "revision": "ca131e6972df0f208a2a",
+    "url": "/Garche_ReactActivity/static/js/runtime~main.ca131e69.js"
+  },
+  {
+    "revision": "b8992aea52845a2523757279c21f5483",
+    "url": "/Garche_ReactActivity/static/media/hero-bg.b8992aea.jpg"
+  },
+  {
+    "revision": "4dc05dfd40bdedd7081ad9e938508e19",
+    "url": "/Garche_ReactActivity/index.html"
+  }
+];
